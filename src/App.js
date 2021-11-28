c453330e03c2cb7b118b59abf62029514cb240ff
Manjlo <calerojhonatan@gmail.com>
2021-11-28 00:01:44 -0500

    change the import order on app.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Characters from './components/cardsCharacters';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Searcher from './components/Searcher';
+import Footer from './components/Footer';
 import { fetchCharacters } from './api_conection/Conection';
 import Pagination from './components/Pagination';
 import { handlePagination } from './api_conection/apiPagination'
 import Modal from './components/Modal';
-import Footer  from './components/Footer';
 
 
 function App() {
   const [url, setUrl] = useState('https://rickandmortyapi.com/api/character/');
   const [characters, setcharacters] = useState([]);
-  const [info, setInfo] = useState({next: null, prev: null});
+  const [info, setInfo] = useState({ next: null, prev: null });
   const [open, setOpen] = useState(false)
   const [modalContent, setModalContent] = useState()
   const [loading, setLoading] = useState(false);
@@ -32,14 +32,14 @@ function App() {
   return (
     <>
 
-      <Navbar setModalContent={setModalContent} setOpen={ setOpen}/>
+      <Navbar setModalContent={setModalContent} setOpen={setOpen} />
       <Header />
       < Searcher updateUrl={updateUrl} />
       <Pagination updateUrl={updateUrl} next={info.next} prev={info.prev} />
-      <Characters characters={characters} setModalContent={setModalContent} setOpen={setOpen} loading={ loading }/>
+      <Characters characters={characters} setModalContent={setModalContent} setOpen={setOpen} loading={loading} />
       <Pagination updateUrl={updateUrl} next={info.next} prev={info.prev} />
       <Modal open={open} setOpen={setOpen} modalContent={modalContent} />
-      <Footer/>
+      <Footer />
     </>
   );
 }
